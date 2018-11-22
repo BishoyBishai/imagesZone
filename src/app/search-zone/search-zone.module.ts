65ebbf6c59558a01aaa7d84958cea6dc84c2a4cc
bishoybishai <bishoy.bishai@gmail.com>
2018-11-22 20:40:55 +0200

    create app using CLI create search module create favorite module install bootstrap

---
+++ src/app/search-zone/search-zone.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SearchZoneRoutingModule } from './search-zone-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SearchZoneRoutingModule
+  ]
+})
+export class SearchZoneModule { }
