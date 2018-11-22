2392409c926245bd985c915db2a10bc4ac9c1d38
bishoybishai <bishoy.bishai@gmail.com>
2018-11-23 00:50:37 +0200

    add app route apply lazy loading for router add header and footer styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: "./search-zone/search-zone.module#SearchZoneModule",
+    pathMatch: "full",
+  },
+  {
+    path: "favorite",
+    loadChildren: "./favorite-zone/favorite-zone.module#FavoriteZoneModule",
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
